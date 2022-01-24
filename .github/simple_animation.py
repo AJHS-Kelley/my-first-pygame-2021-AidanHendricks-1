1df2275571064edf348cebce271e91cc60968db4
AidanHendricks <90405653+AidanHendricks@users.noreply.github.com>
2022-01-24 11:36:28 -0500

    bugfix
    fixed a part where the shapes go a little past the right screen.

--- .github/simple_animation.py
+++ .github/simple_animation.py
@@ -75,7 +75,7 @@ while True:
                 b['dir'] = DOWNRIGHT
             if b['dir'] == UPLEFT:
                 b['dir'] = UPRIGHT
-        if b['rect'].left > WINDOWWIDTH:
+        if b['rect'].right > WINDOWWIDTH:
             # The box has moved past the right
             if b['dir'] == DOWNRIGHT:
                 b['dir'] = DOWNLEFT
